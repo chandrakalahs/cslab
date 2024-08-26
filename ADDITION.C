51ae9d9957601d65074012f9030e2e01364b61d9
chandrakalahs <115870285+chandrakalahs@users.noreply.github.com>
2024-08-26 10:01:04 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum,sub,mul,div;
+int  *p1,*p2;
+clrscr();
+printf("\nENTER THE VALUES FOR A&B:");
+scanf("%d%d",&a,&b);
+p1=&a;
+p2=&b;
+sum=*p1+*p2;
+sub=*p1-*p2;
+mul=*p1 * *p2;
+div=*p1/*p2;
+printf("\nSUM OF TWO NUMBERS=%d",sum);
+printf("\nSUBTRACTION OF TWO NUMBERS=%d",sub);
+printf("\nMULTIPLICATION OF TWO NUMBERS=%d",mul);
+printf("\nDIVISION OF TWO NUMBERS=%d",div);
+getch();
+}
+
+
+
+
+
+
