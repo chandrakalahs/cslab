2b1f2571d5502c0250ca96e893106b36e3b0cc19
chandrakalahs <115870285+chandrakalahs@users.noreply.github.com>
2024-08-26 10:27:50 +0530

    Add files via upload

---
+++ EXAMPLE.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum,sub,mul,div;
+int  *p1,*p2;
+clrscr();
+printf("\nENTER THE VALUES FOR A&B:");
+scanf("%d%d",&a,&b);
+p1=&a;
+p2=&b;
+sum=*p1+*p2;
+sub=*p1-*p2;
+mul=*p1 * *p2;
+div=*p1/
+
+
+
+
+
+
